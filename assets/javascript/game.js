3a3a3b480eb1c547ff8da1ff9dd2dba1d4c1648a
Ralph Manlapig <Ralph Manlapig>
2017-02-12 12:11:05 -0500

    updating js page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,52 @@
-$(document).ready(function() {
-
 var randomNum = Math.round(Math.random() * 120)+19;
 
+var gemNums =[];
+
+
+
+
+$(document).ready(function() {
 
 console.log(randomNum);
 
 $("#randomNumber").html("Your number to get: " + randomNum);
 
-gemNums =[];
-
 for (i=0; i < 4; i++){
 	gemNums[i] = Math.round(Math.random() * 12)+1;
-	$("#gem + i").attr("value", gemNums[i]);
-	console.log(#gem[i].value);
+	$("#gem" + i).attr("value", gemNums[i]);
 
 }
 
-console.log(gemNums);
-
-
-
-});
-
-var total = 0;
-
+//$(document).on("click", ".gems", function() {
 $(".gems").on("click", function() {
-	alert("clicked");
+	var total = 0;
+
 	total += this.value;
-	console.log(total);
+
+	// total += $(this).attr("value");
+	console.log(this.value);
+
+	$("#score").html(total);
+
+	if (total > window.randomNum){
+	losses++;
+	alert("you lose! try again!");
+	$("#losses").html("Losses: " + losses);
+}
+	if (total === randomNum){
+	wins++;
+	alert("you win!");
+	("#wins").html("Wins: " + wins);
+}
 
 });
+
+});
+
+
+
+
+
 
 
 
